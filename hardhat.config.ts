82d95ba5e26a17450a20cd51d3fba3152420ff19
igorsenych-cw <igor.senych@cloudwalk.io>
2022-11-29 15:32:44 +0200

    Configure environment

---
+++ hardhat.config.ts
@@ -0,0 +1,40 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk"
+      }
+    },
+    substrate: {
+      url: "http://127.0.0.1:9933",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+      gas: "auto"
+    },
+  },
+  mocha: {
+    timeout: 120000
+  },
+};
+
+export default config;
