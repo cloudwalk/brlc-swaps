58754782674786ae69393ecc68800ea7e2e09b68
Evgenii Zaitsev <evgenii@cloudwalk.io>
2023-01-25 00:48:21 +0700

    Develop tests for the SignatureChecker contract

---
+++ contracts/mock/SignatureCheckerMock.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.16;
+
+import { SignatureChecker } from "../base/SignatureChecker.sol";
+
+/**
+ * @title PausableExtUpgradeableMock contract
+ * @author CloudWalk Inc.
+ * @dev An implementation of the {SignatureChecker} base contract for test purposes.
+ */
+contract SignatureCheckerMock is SignatureChecker  {
+    /// @dev Calls the appropriate function of the base contract
+    function splitSignature(bytes memory sig) external pure returns (uint8, bytes32, bytes32) {
+        return _splitSignature(sig);
+    }
+
+    /// @dev Calls the appropriate function of the base contract
+    function recoverSigner(bytes32 message, bytes memory sig) external pure returns (address) {
+        bytes32 messageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", message));
+        return _recoverSigner(messageHash, sig);
+    }
+}
